809cbdeaa4b59a36c2d3d27e69bbce1349105a7c
Pankaj Sharma <psharma@incurrency.com>
2015-07-24 10:50:21 +0530

    changes to ExtendedHashMap so that it can be used as an inmemory key value store

--- src/com/incurrency/framework/ExtendedHashMap.java
+++ src/com/incurrency/framework/ExtendedHashMap.java
@@ -14,6 +14,10 @@ public class ExtendedHashMap<J, K, V> {
 
     public HashMap<J, HashMap<K, V>> store = new HashMap<>();
 
+    public void put(J key, HashMap<K,V> map) {
+            store.put(key, map);            
+    }
+    
     public void add(J key, K subkey, V value) {
         if (store.get(key) == null) {
             HashMap<K, V> temp = new HashMap<>();
@@ -22,5 +26,23 @@ public class ExtendedHashMap<J, K, V> {
         } else {
             store.get(key).put(subkey, value);
         }
+    }    
+    
+    
+    public V get(J key, K subkey) {
+        if (store.get(key) == null) {
+            return null;
+        } else {
+            return store.get(key).get(subkey);
+        }
     }
+    
+    public HashMap <K,V> get(J key) {
+        if (store.get(key) == null) {
+            return null;
+        } else {
+            return store.get(key);
+        }
+    }
+    
 }
