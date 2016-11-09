ab6146993bd46653443b35b82af6a9598255a0a1
Pankaj Sharma <psharma@incurrency.com>
2016-11-09 03:31:32 +0100

    Strategy: getLongOnly() and getShortOnly() now take effect on both entry and exit, even if the extending strategy has not implemented the logic for pausing a set of trades

--- src/com/incurrency/framework/Strategy.java
+++ src/com/incurrency/framework/Strategy.java
@@ -696,11 +696,14 @@ public class Strategy implements NotificationListener {
     }
 
     public int entry(HashMap<String, Object> order) {
+        EnumOrderSide side = EnumOrderSide.valueOf(order.get("side") != null ? order.get("side").toString() : "UNDEFINED");
+        if((!getLongOnly() && side.equals(EnumOrderSide.BUY))||(!getShortOnly() && side.equals(EnumOrderSide.SHORT))){
+            return -1;
+        }
         int id = Integer.valueOf(order.get("id").toString());
         int size = Utilities.getInt(order.get("size"), 0);
         order.put("orderref", this.getStrategy());
         double limitPrice = Utilities.getDouble(order.get("limitprice").toString(), 0);
-        EnumOrderSide side = EnumOrderSide.valueOf(order.get("side") != null ? order.get("side").toString() : "UNDEFINED");
         if (id >= 0) {
             size = size == 0 && getNumberOfContracts() == 0 ? (int) (getExposure() / limitPrice) : size == 0 ? getNumberOfContracts() * Parameters.symbol.get(id).getMinsize() : size;
             order.put("size", size);
@@ -746,14 +749,18 @@ public class Strategy implements NotificationListener {
         } else {
             return -1;
         }
+        
     }
 
     public synchronized void exit(HashMap<String, Object> order) {
+        EnumOrderSide side = EnumOrderSide.valueOf(order.get("side") != null ? order.get("side").toString() : "UNDEFINED");
+        if((!getLongOnly() && side.equals(EnumOrderSide.COVER))||(!getShortOnly() && side.equals(EnumOrderSide.SELL))){
+            return;
+        }
         int id = Integer.valueOf(order.get("id").toString());
         int size = Utilities.getInt(order.get("size"), 0);
         order.put("orderref", this.getStrategy());
         double limitPrice = Utilities.getDouble(order.get("limitprice"), 0);
-        EnumOrderSide side = EnumOrderSide.valueOf(order.get("side") != null ? order.get("side").toString() : "UNDEFINED");
         Boolean scaleout = order.get("scale") != null ? Boolean.valueOf(order.get("scale").toString()) : false;
         EnumOrderReason reason = EnumOrderReason.valueOf(order.get("reason") != null ? order.get("reason").toString() : "UNDEFINED");
         if (id >= 0) {
