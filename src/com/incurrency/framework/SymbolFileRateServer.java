7f4cef3e7bf426b63f4ea3c5e8149e92e1d40069
root <root@notanotherfund.com>
2017-05-08 11:32:58 +0530

    Added BANKNIFTY Index

--- src/com/incurrency/framework/SymbolFileRateServer.java
+++ src/com/incurrency/framework/SymbolFileRateServer.java
@@ -61,6 +61,14 @@ public class SymbolFileRateServer {
         s.setStreamingpriority(1);
         s.setStrategy("DATA");
         out.add(s);
+        s = new BeanSymbol("BANKNIFTY", "BANKNIFTY", "IND", "", "", "");
+        s.setCurrency("INR");
+        s.setExchange("NSE");
+        s.setStreamingpriority(1);
+        s.setStrategy("DATA");
+        s.setMinsize(40);
+        s.setStrikeDistance(100);
+        out.add(s);
         s = new BeanSymbol("NIFTY50", "NSENIFTY", "FUT", expiry, "", "");
         s.setCurrency("INR");
         s.setExchange("NSE");
